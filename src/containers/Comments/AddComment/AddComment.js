6f2c50c39a8034be4222083b77be5f572dbd1a04
DNH1111 <101248255+DNH1111@users.noreply.github.com>
2023-02-28 11:07:35 +0900

    Update AddComment.js

--- src/containers/Comments/AddComment/AddComment.js
+++ src/containers/Comments/AddComment/AddComment.js
@@ -5,5 +5,8 @@ import {Form, Image, TextArea} from "semantic-ui-react";
 export function AddComment() {
   return (
     <div className='add-comment'>
+      <Form>
+        <Form.TextArea control={TextArea} autoHeight placeholder='Add a public comment' />
+      </Form>
   );
 }
