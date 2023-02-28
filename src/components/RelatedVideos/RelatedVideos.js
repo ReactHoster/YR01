aa25419ccff68370ca1601dd48e2cfed3522da09
DNH1111 <101248255+DNH1111@users.noreply.github.com>
2023-02-28 11:38:48 +0900

    Update RelatedVideos.js

--- src/components/RelatedVideos/RelatedVideos.js
+++ src/components/RelatedVideos/RelatedVideos.js
@@ -4,9 +4,6 @@ import './RelatedVideos.scss';
 import {NextUpVideo} from './NextUpVideo/NextUpVideo';
 
 export function RelatedVideos(props) {
-  if (!props.videos || !props.videos.length) {
-    return <div className='related-videos'/>;
-  }
 
   // safe because before we check if the array has at least one element
   const nextUpVideo = props.videos[0];
