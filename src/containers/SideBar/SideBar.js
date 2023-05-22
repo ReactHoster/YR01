4989f618915d5dc6a2a3cc16bbf5dc6c7cd540fb
DNH1111 <101248255+DNH1111@users.noreply.github.com>
2023-05-22 14:03:40 +0900

    Update SideBar.js

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -10,9 +10,9 @@ export class SideBar extends React.Component {
   render() {
     return (
       <Menu borderless vertical stackable fixed='left' className='side-nav'>
-        <SideBarItem path='/' label='Home' icon='home'/>
-        <SideBarItem path='/feed/trending' label='Trending' icon='fire'/>
-        <SideBarItem label='Followers' icon='spy'/>
+        <SideBarItem path='/' label='ホーム' icon='home'/>
+        <SideBarItem path='/feed/trending' label='トレンド' icon='fire'/>
+        <SideBarItem label='フォロワー' icon='spy'/>
         <Divider/>
         <SideBarHeader title='Library'/>
         <SideBarItem label='History' icon='history'/>
